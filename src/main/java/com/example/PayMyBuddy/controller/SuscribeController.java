cc911ae6b3e296f93401f01f07c1f716dcc7eb9d
Dixen7 <dixenlive@gmail.com>
2022-02-24 17:18:32 +0100

    delete useless authowired

--- src/main/java/com/example/PayMyBuddy/controller/SuscribeController.java
+++ src/main/java/com/example/PayMyBuddy/controller/SuscribeController.java
@@ -20,7 +20,6 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class SuscribeController {
 
-    @Autowired
     UserServiceInterface userServiceInterface;
 
     RoleServiceInterface roleServiceInterface;

5778ad8911270bc7e254dbfffa130a150e9778cf
Dixen7 <dixenlive@gmail.com>
2022-03-30 14:55:05 +0200

    fix dependancy accountServiceTest

--- src/test/java/com/example/PayMyBuddy/Service/AccountServiceTest.java
+++ src/test/java/com/example/PayMyBuddy/Service/AccountServiceTest.java
@@ -1,20 +1,10 @@
 package com.example.PayMyBuddy.Service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-
-import com.example.PayMyBuddy.model.Account;
-import com.example.PayMyBuddy.model.BankAccount;
-import com.example.PayMyBuddy.model.Role;
 import com.example.PayMyBuddy.model.User;
 import com.example.PayMyBuddy.repository.AccountRepository;
 import com.example.PayMyBuddy.service.AccountService;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
